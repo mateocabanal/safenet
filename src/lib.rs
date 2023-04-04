1358ae2cc7670bb065d87d4d953108c459a532d8
Mateo Cabanal <mateo@mateocabanal.ca>
2023-04-03 21:43:41 -0700

    test has regressed due to chat example

--- src/lib.rs
+++ src/lib.rs
@@ -176,11 +176,11 @@ mod tests {
     fn test_uuid() -> Result<(), Box<dyn std::error::Error>> {
         start_http_server();
         while !APPSTATE.read()?.is_http_server_on {}
-        let app_state = APPSTATE.read()?;
-        let init_res = crate::client::http::start_tunnel("127.0.0.0.1:3876".parse()?)?;
-        assert_eq!(app_state.uuid, uuid::Uuid::from_slice(&init_res.as_bytes()[3..=18])?);
+        let uuid = APPSTATE.read()?.uuid.clone();
+        let init_res = crate::client::http::start_tunnel("127.0.0.1:3876".parse()?)?;
+        assert_eq!(uuid, uuid::Uuid::from_slice(&init_res.as_bytes()[3..=18])?);
         let res = crate::client::http::msg("127.0.0.1:3876".parse()?, "test")?;
-        assert_eq!(app_state.uuid, uuid::Uuid::from_slice(&res.as_bytes()[3..=18])?);
+        assert_eq!(uuid, uuid::Uuid::from_slice(&res.as_bytes()[3..=18])?);
 
         Ok(())
     }
